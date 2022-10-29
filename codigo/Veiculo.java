4cee75f1914b62b90a0b61bfe47b6d8ddbbb2a2c
Rafaella <rafacssacrament@gmail.com>
2022-10-29 12:13:06 -0300

    adicionando os atributos e metodos de acordo com o diagrama

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -3,11 +3,20 @@ package app;
 public class Veiculo {
 	protected double capacidade;
 	protected double km_medio;
-	
+	protected double tanque;
+	protected double valor_venda;
+
 	public double autonomia_diaria() {
-		return (Double) null;
+		return 0;
 	}
 	
 	public void rota_valida() {
 	}
+	public void seguro(){
+
+	}
+	public void IPVA(){
+
+	}
+
 }
