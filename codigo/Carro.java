d71b02b1c5eea1eac5b9ca1361a16982d4523762
Rafaella <rafacssacrament@gmail.com>
2022-10-29 14:28:54 -0300

    enviando a quilometragem especifica pelo construtor

--- codigo/Carro.java
+++ codigo/Carro.java
@@ -1,7 +1,12 @@
 package app;
 
 public class Carro extends Veiculo{
+	static double capacidade = 50;
+	Carro(double km_medio, double tanque, double valor_venda){
+		super(capacidade, km_medio, tanque, valor_venda);
+	}
+	
 	public double autonomia_diaria() {
-		return (Double) null;
+		return 0;
 	}
 }
