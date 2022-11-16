47fe00cc1cc945e946a0a7bcc0a3cba169097b32
PAlucas <lucaslotti300@gmail.com>
2022-11-15 23:09:53 -0300

    adicionando leitor de csv

--- codigo/App.java
+++ codigo/App.java
@@ -1,8 +1,9 @@
 package codigo;
+
 public class App {
 	public static void main(String[] args){
-		
-	
+		CsvReader leitor = new CsvReader("codigo/Pasta1.csv");
+		System.out.print(leitor.returnArrayNumbers());
 	}
 }
 
---
+++ codigo/CsvReader.java
@@ -0,0 +1,55 @@
+package codigo;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Classe que lê arquivo csv, nela você coloca no construtor o caminho
+ * do arquivo csv e a função returnArrayNumber retorna um ArrayList com
+ * todos os elementos do csv.
+ */
+public class CsvReader{
+    private String caminhoRelativoArquivoCsv;
+    public CsvReader(String caminhoRelativo){
+        this.caminhoRelativoArquivoCsv = caminhoRelativo;
+    }
+
+    public List<Integer> returnArrayNumbers(){
+        String arquivoCSV = "codigo/Pasta1.csv";
+		BufferedReader br = null;
+		String linha = "";
+		String csvDivisor = ";";
+		try {
+            List<Integer> numeros = new ArrayList<Integer>();
+			br = new BufferedReader(new FileReader(arquivoCSV));
+			while ((linha = br.readLine()) != null) {
+	
+				String[] valores = linha.split(csvDivisor);
+	
+				for (String string : valores) {
+                    numeros.add(Integer.parseInt(string));
+                }
+	
+			}
+            return numeros;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+        return null;
+    }
+}
