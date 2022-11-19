51e57fe01d4d66a828f7507c70825de509c8bd72
Rafaella <rafacssacrament@gmail.com>
2022-11-19 20:33:38 -0300

    criando o enum combustivel

---
+++ codigo/Combustivel.java
@@ -0,0 +1,17 @@
+package codigo;
+/**
+ * O enum combustivel serve para armazenar os dados de cada tipo de combustivel
+ * cada combustivel tem dois dados especificos, sendo o primeiro deles a quilometragem por livro
+ * e o segundo, o preço daquele combustivel por litro. 
+  */
+public enum Combustivel {
+    Gasolina(12.0,4.80), Etanol(8.0,3.65), Diesel(3.5, 6,65);
+    private double quilometragemPorLitro;
+    private double precoDoLitro;
+
+    Combustivel(double quilometragemPorLitro, double precoDoLitro) {
+        this.quilometragemPorLitro = quilometragemPorLitro;
+        this.precoDoLitro = precoDoLitro;
+     }
+}
+ 
