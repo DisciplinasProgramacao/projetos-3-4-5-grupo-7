9f4f0ff27b53d35121c05a2281b1dbf765d9063c
Rafaella <rafacssacrament@gmail.com>
2022-11-19 22:24:21 -0300

    iniciando a criacao da classe carro

--- codigo/Carro.java
+++ codigo/Carro.java
@@ -1,9 +1,12 @@
 package codigo;
 
 public class Carro extends Veiculo {
-	Carro(double km_medio, double tanque, double valor_venda) {
-		super(km_medio, tanque, valor_venda);
-		capacidade = 50; 
+	Carro(double km_medio, double tanque, double valor_venda, double preco_ipva, double preco_seguro) {
+		super(km_medio, tanque, valor_venda, preco_ipva, preco_seguro);
 	}
+	public void adicionar_combustivel(){
+	}
+	public void contar_quant_rotas_por_veiculo(){
 
+	}
 }
