37fb46547712804c820f9419b3a2b4c831306fe0
LucasLotti <lucaslotti300@gmail.com>
2022-11-19 23:54:25 -0300

    adicionar arquivos

--- codigo/App.java
+++ codigo/App.java
@@ -1,9 +1,47 @@
 package codigo;
 
+import java.util.Scanner;
+
 public class App {
-	public static void main(String[] args){
-		CsvReader leitor = new CsvReader("codigo/Pasta1.csv");
-		System.out.print(leitor.returnArrayNumbers());
+	public static void main(String[] args) throws Exception {
+		Scanner teclado = new Scanner(System.in); // para ler o teclado
+		int opcao = 1;// escolha da operação pelo usuário
+		String nome = "";
+		Frota frota = new Frota();
+
+		do {
+			System.out.println("Frota de carros");
+			System.out.println("1 - Carregar um conjunto de veículos de um arquivo");
+			System.out.println("2 - Salvar um conjunto de veículos de um arquivo");
+			System.out.println("3 - Incluir Novo veículo");
+			System.out.println("3 - Localizar ");
+			System.out.println("0 - Sair");
+			System.out.println("Digite opção");
+			opcao = Integer.parseInt(teclado.nextLine());
+
+			if (opcao == 0) { // termina o sistema se a opÃ§Ã£o for 0
+				System.out.println("Obrigado e volte sempre!");
+				teclado.close();
+				return;
+			}
+			switch (opcao) {
+				case 1:
+					System.out.println("Colocar Arquivo");
+					System.out.println(frota.addVeiculos("codigo/Pasta1.csv"));
+					System.out.println("Compromisso Marcado");
+					break;
+				case 2:
+					System.out.println("agenda.relatorioCompromissos()");
+					break;
+				case 3:
+					System.out.println("Colocar dia");
+					nome = teclado.nextLine();
+
+					break;
+				case 0:
+					return;
+			}
+		} while (opcao != 0); // repete-se atÃ© que o usuÃ¡rio queira sair.
+		teclado.close();
 	}
 }
-
---
+++ codigo/Frota.java
@@ -0,0 +1,53 @@
+package codigo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frota {
+    private List<Veiculo> listaVeiculos = new ArrayList<Veiculo>();
+
+    public double calcular_Media(){
+        double totalKm = this.listaVeiculos.stream().mapToDouble( veiculoKm -> veiculoKm.getRota().stream().mapToDouble(rota -> rota.distancia_total).sum()).sum();
+        int quantidadeDeRotas = this.listaVeiculos.stream().mapToInt(totalRotaPorVeiculo -> totalRotaPorVeiculo.getRota().size()).sum();
+        return totalKm/quantidadeDeRotas;
+    }
+
+    public void addVeiculo(Veiculo veiculo){
+        listaVeiculos.add(veiculo);
+    }
+
+    public String addVeiculos(String arquivo){
+        CsvReader newVeiculos = new CsvReader(arquivo);
+        for (String veiculo : newVeiculos.returnArrayNumbers()) {
+            String[] veiculosInfo = veiculo.split(",");
+            switch (veiculosInfo[0]) {
+                case "Caminhao":
+                    Caminhao caminhaoAux = new Caminhao(Double.parseDouble(veiculosInfo[1]), Double.parseDouble(veiculosInfo[2]), Double.parseDouble(veiculosInfo[3]), Double.parseDouble(veiculosInfo[4]), Double.parseDouble(veiculosInfo[4]));
+                    listaVeiculos.add(caminhaoAux);
+                case "Carro":
+                    Carro carroAux = new Carro(Double.parseDouble(veiculosInfo[1]), Double.parseDouble(veiculosInfo[2]), Double.parseDouble(veiculosInfo[3]));
+                    listaVeiculos.add(carroAux);
+                case "Van":
+                    Van vanAux = new Van(Double.parseDouble(veiculosInfo[1]), Double.parseDouble(veiculosInfo[2]), Double.parseDouble(veiculosInfo[3]));
+                    listaVeiculos.add(vanAux);
+                case "Furgao":
+                    Furgao forgaoAux = new Furgao(Double.parseDouble(veiculosInfo[1]), Double.parseDouble(veiculosInfo[2]), Double.parseDouble(veiculosInfo[3]));
+                    listaVeiculos.add(forgaoAux);
+            }
+        }
+        return "nao possui veiculos";
+    }
+
+    public void saveVeiculos(Veiculo veiculo, String arquivo){
+        StringBuilder stringParaArquivoFinal = new StringBuilder();
+        for (int i = 0; i < listaVeiculos.size(); i++) {
+            stringParaArquivoFinal.append(listaVeiculos.getClass()).append(",").append(listaVeiculos.get(i).getKm_medio()).append(",").append(listaVeiculos.get(i).getValor_venda()).append(",").append(listaVeiculos.get(i).preco_seguro).append(";");
+        }
+        
+    }
+
+    public double[] ordem_decrescente_por_custo(){
+        double[] sort = this.listaVeiculos.stream().mapToDouble( veiculoKm -> veiculoKm.custos()).sorted().toArray();
+        return sort;
+    }
+}
