723f77141934c92d5db5966ebee0feb9681ee62a
Vítor Lion <vitorxbox6@gmail.com>
2022-11-19 23:41:17 -0300

    Carro e caminhão corretamente

--- codigo/Caminhao.java
+++ codigo/Caminhao.java
@@ -1,10 +1,26 @@
 package codigo;
 
-public class Caminhao extends Veiculo{
-	public double autonomia_diaria() {
-		return (Double) null;
+public class Caminhao extends Veiculo {
+	Caminhao(double km_medio, double tanque, double valor_venda, double preco_ipva, double preco_seguro) {
+		super(km_medio, tanque, valor_venda, preco_ipva, preco_seguro);
+		// TODO Auto-generated constructor stub
 	}
-	public int vistoria() {
-		return 0;
+
+
+	public void adicionar_combustivel(double litros){
+
+		if (this.tanque < this.capacidade) {
+			this.tanque =+ litros;
+		} else {
+			System.out.println("O tanque está cheio");
+		}
+	}
+
+	@Override
+	public void adicionar_combustivel() {
+	}
+
+	@Override
+	public void contar_quant_rotas_por_veiculo() {
 	}
 }
--- codigo/Carro.java
+++ codigo/Carro.java
@@ -1,9 +1,25 @@
 package codigo;
 
 public class Carro extends Veiculo {
-	Carro(double km_medio, double tanque, double valor_venda) {
-		super(km_medio, tanque, valor_venda);
-		capacidade = 50; 
+	Carro(double km_medio, double tanque, double valor_venda, double preco_ipva, double preco_seguro) {
+		super(km_medio, tanque, valor_venda, preco_ipva, preco_seguro);
+		//TODO Auto-generated constructor stub
 	}
 
+	public void adicionar_combustivel(double litros) {
+
+	if (this.tanque < this.capacidade) {
+		this.tanque =+ litros;
+	} else {
+		System.out.println("O tanque está cheio");
+	}
+}
+
+	@Override
+	public void adicionar_combustivel() {
+	}
+
+	@Override
+	public void contar_quant_rotas_por_veiculo() {
+	}
 }
